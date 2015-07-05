fbac7e034effff8228e35aeef0f6702ab86f9f13
Alexander Stram <astram@sailfish.(none)>
2015-07-04 21:27:43 -0700

    removing unnecessary includes

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,18 @@
 #define LINE_LEN	1024 
 #define __STDC_LIMIT_MACROS
 
-//#include <cstdlib>
-//#include <fstream>
 #include <iostream>
 #include <algorithm> //for sort
 #include <sstream>
 #include <vector>
 #include <time.h>
-//#include <getopt.h>
 #include <math.h>
 #include <string.h>
 #include <assert.h>
 #include <mpi.h>
 #include <float.h>
-//#include <iomanip>
-//#include <stdint.h>
 #include <dlfcn.h>
-//#include <stdlib.h>
 #include <sys/stat.h>
-//#include <sys/types.h>
-//#include <unistd.h>
 
 #include "rapidxml/rapidxml.hpp"
 
